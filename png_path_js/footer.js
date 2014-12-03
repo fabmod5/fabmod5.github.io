c12bb1fa586ef2b0604458ebb1d9859307a67b39
Shohei Aoki <shoaok@gmail.com>
2014-12-03 15:57:22 +0900

    implement blob transfering

--- png_path_js/footer.js
+++ png_path_js/footer.js
@@ -5,20 +5,19 @@
            console.log(path);
 
            var path_blob = new Blob([path]);
-           document.path_blob = path_blob; 
-           var a = document.createElement("a");
-           var label = document.createTextNode("Download");
-           var disp = document.getElementById("disp");
-
-           if (window.URL) {
-             disp.innerHTML = '<a href="' + window.URL.createObjectURL(path_blob) + '" target="_blank">Download .path file</a>';
-           } else if (window.webkitURL) {
-             disp.innerHTML = '<a href="' + window.webkitURL.createObjectURL(path_blob) + '" target="_blank">Download .path file</a>';
+           document.path_blob = path_blob;
+           // var a = document.createElement("a");
+           // var label = document.createTextNode("Download");
+           // var disp = document.getElementById("disp");
+           //
+           // if (window.URL) {
+           //   disp.innerHTML = '<a href="' + window.URL.createObjectURL(path_blob) + '" target="_blank">Download .path file</a>';
+           // } else if (window.webkitURL) {
+           //   disp.innerHTML = '<a href="' + window.webkitURL.createObjectURL(path_blob) + '" target="_blank">Download .path file</a>';
+           clearInterval(timerId);
            } else {
             console.log("processing png_path ...");
            }
-           clearInterval(timerId);
-       }
     },300);
 
 }
