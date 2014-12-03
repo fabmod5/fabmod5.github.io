e78f36bb493c3814376a923ad36a9d74f859040b
Shohei Aoki <shoaok@gmail.com>
2014-12-03 19:05:58 +0900

    add path view, should be fixed

--- png_path_js/footer.js
+++ png_path_js/footer.js
@@ -1,11 +1,43 @@
+
+var drawPath = function(path){
+    var canvas = document.getElementById('pathview');
+    var ctx = canvas.getContext( "2d" );
+    var px,py,cx,cy = 0;
+    function drawLine(cx,cy,px,py){
+           ctx.beginPath();
+           ctx.moveTo( px, py);
+           ctx.lineTo( cx, cy);
+           ctx.closePath();
+           ctx.stroke();
+    }
+    var arr = path.split('\n');
+    for (var i = 0; i < arr.length; i++) { 
+      if(arr[i].match(/^\d/)!=null) {
+        var coords = arr[i];
+        cx = coords.split(' ')[0]/3.5;
+        cy = coords.split(' ')[1]/3.5;
+        drawLine(cx,cy,px,py)
+        px = cx;
+        py = cy;
+      }
+    }
+}
+
  var timerId = setInterval(function(){
        if(FS.findObject("/output").contents !== null){
            console.log("Output file ready."); 
            var path = String.fromCharCode.apply(null, Module.ret());
+           document.path = path; 
            // console.log(path);
+
+           drawPath(path);
 
            var path_blob = new Blob([path]);
            document.path_blob = path_blob;
+
+           // var stat = document.getElementById("stat");
+           // stat.innerHTML = '<p>Done.</p>';
+           
            // var a = document.createElement("a");
            // var label = document.createTextNode("Download");
            // var disp = document.getElementById("disp");
